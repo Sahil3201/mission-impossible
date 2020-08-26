27544f6eafb48a4dc542cbf23d1f0403aad20a67
Sahil Lunawat <45628585+Sahil3201@users.noreply.github.com>
2020-08-26 21:55:02 +0530

    Set debug to false

--- missionimpossible/settings.py
+++ missionimpossible/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'v(aajh48^r4gw9zcodmw@+4_!6y$ya+bvsujo-hom7wvrp#drg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
